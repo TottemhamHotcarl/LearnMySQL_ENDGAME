c7d25cb2480a8dd5f25e2f3f5be5c3d86e98ae30
Carl Ginster <1606558@students.wits.ac.za>
2019-05-05 18:01:23 +0200

    Update Sprint1Test.java

--- src/test/java/Sprint1Test.java
+++ src/test/java/Sprint1Test.java
@@ -1,9 +1,15 @@
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+
+import com.example.LearnMySQL_Final.ServerManagementConnection;
 
 public class Sprint1Test {
 	
-	@Test public void Student_Login_Interface_First_Time_login() {
-		
+	@Test public void Student_Login_Interface_First_Time_login_True() {
+		ServerManagementConnection smc = new ServerManagementConnection("127.0.0.1");
+		boolean b= smc.isFirstTime("1606558");
+		assertTrue(b);
 	}
 
 }
