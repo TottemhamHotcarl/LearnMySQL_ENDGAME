9a1df74306435ba73e55b16f322dd1fce720cc5b
Carl Ginster <1606558@students.wits.ac.za>
2019-05-05 18:53:54 +0200

    Add files via upload

--- src/test/java/Sprint1Test.java
+++ src/test/java/Sprint1Test.java
@@ -1,15 +1,31 @@
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import com.example.LearnMySQL_Final.Person;
 import com.example.LearnMySQL_Final.ServerManagementConnection;
 
 public class Sprint1Test {
 	
+	
+	ServerManagementConnection smc = new ServerManagementConnection("127.0.0.1");
+	Person p = new Person("Carl Ginster","Carl","Ginster","1606558@students.wits.ac.za","1606558","1");
 	@Test public void Student_Login_Interface_First_Time_login_True() {
-		ServerManagementConnection smc = new ServerManagementConnection("127.0.0.1");
+		
 		boolean b= smc.isFirstTime("1606558");
 		assertTrue(b);
 	}
+	
+	
+	@Test public void Student_Login_Interface_First_Time_login_False() {
+		boolean b= smc.isFirstTime("1606443");
+		assertFalse(b);
+	}
+	@Test public void Student_Login_Interface_Add_To_Databases() {
+		boolean b= smc.addStudentToStudentTableInDatabase(p);
+		assertTrue(b);
+	}
+	
 
 }
