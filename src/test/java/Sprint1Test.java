6604936ae27e8dd6ec3c8f21d26a2586a0bb243a
Carl Ginster <1606558@students.wits.ac.za>
2019-05-27 16:51:37 +0200

    Update Sprint1Test.java

--- src/test/java/Sprint1Test.java
+++ src/test/java/Sprint1Test.java
@@ -13,6 +13,7 @@ public class Sprint1Test {
 	Person p = new Person("Carl Ginster","Carl","Ginster","1606558@students.wits.ac.za","1606558","1");
 	Person testPerson = new Person("Test","Test","Test","Test@students.wits.ac.za","1","1");
 	Person testPerson2 = new Person("Test","Test","Test","Test@students.wits.ac.za","2","1");
+	
 	@Test public void Student_Login_Interface_First_Time_login_True() {
 		boolean b= smc.isFirstTime("1606558");
 		assertTrue(b);
@@ -24,12 +25,25 @@ public class Sprint1Test {
 		assertFalse(b);
 	}
 	
+	
+	// test for user
 	@Test public void User_Set_Person() {
 		User ur = new User();
 		ur.setperson(p);
 		User ur2 = new User();
 		assertTrue(ur2.person.getId().equals("1606558"));
 	}
+	
+	@Test public void User_Set_Person_constructor() {
+		User ur = new User(p);
+		User ur2 = new User();
+		assertTrue(ur2.person.getId().equals("1606558"));
+	}
+	
+	//test for Person
+	
+	
+	
 	
 
 }
