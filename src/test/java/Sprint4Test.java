6838b0cbe4d2f4b32c8f4429cf0d6d262f6a0b61
Carl Ginster <1606558@students.wits.ac.za>
2019-05-27 19:54:28 +0200

    Update Sprint4Test.java

--- src/test/java/Sprint4Test.java
+++ src/test/java/Sprint4Test.java
@@ -140,6 +140,15 @@ public class Sprint4Test {
 		assertTrue(b);
 	}
 	
+	@Test public void GSMC_addRemove_Test() {
+		GroupObject go = new GroupObject("27","1606558","Super Group","d1606558");
+		Person ptest = new Person("Test Tesy","Test","Tesy","Testy@students.wits.ac.za",Integer.toString(rm.nextInt(200000)),"1");
+		smc.addStudentToStudentTableInDatabase(ptest);
+		boolean b = gsmc.addMember(ptest.getId());
+		boolean b2 = gsmc.removeMember(ptest.getId());
+		assertTrue(b&&b2);
+	}
+	
 	
 	
 	
